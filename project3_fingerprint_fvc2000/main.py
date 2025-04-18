4fb7be342414c227f26229bc79e8fce29e82ce4a
Zoe27 <husiyu1995@gmail.com>
2025-04-18 16:13:46 +0200

    update preprocess

---
+++ project3_fingerprint_fvc2000/main.py
@@ -0,0 +1,6 @@
+from preprocess import batch_preprocess_images
+
+input_folder = "./project3_fingerprint_fvc2000/data/DB1_B"
+output_folder = "./project3_fingerprint_fvc2000/data/processed/DB1_B"
+
+batch_preprocess_images(input_folder, output_folder)
